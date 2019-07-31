aa6abc126dfd781f2bb2c8c9c72bb6e0d2917b79
Eric Hollander <hh@hh-HP-Z440-Workstation>
2019-07-30 17:38:26 -0700

    more minor fixes

--- webapp/src/main/java/chiralsoftware/webapp/controllers/FileUploadController.java
+++ webapp/src/main/java/chiralsoftware/webapp/controllers/FileUploadController.java
@@ -3,7 +3,7 @@ package chiralsoftware.webapp.controllers;
 import entities.TemporaryImage;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.logging.Level;
+import static java.util.logging.Level.INFO;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,13 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -36,16 +36,10 @@ public class FileUploadController {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @RequestMapping(value = "/upload.htm", method = RequestMethod.POST)
+    @PostMapping(value = "/upload.htm")
     @ResponseBody
     @Transactional
-    public String testUpload(HttpServletRequest request,
-            HttpServletResponse response) {
-        if(request.getSession().getServletContext().getMajorVersion() < 3) {
-            LOG.info("Warning! The servlet API version is less than 3 and so this won't work!");
-            return null;
-        }
-
+    public String testUpload(HttpServletRequest request, HttpServletResponse response) {
         try {
             final Collection<Part> parts = request.getParts();
             LOG.info("I have this many parts: " + parts.size());
@@ -60,14 +54,14 @@ public class FileUploadController {
                 return temporaryImage.getId().toString();
             }
         } catch (IOException | ServletException e) {
-            LOG.log(Level.INFO, "Caught an exception: ", e);
+            LOG.log(INFO, "Caught an exception: ", e);
         }
         return "hellothere";
     }
 
     /** This is a test that shows JPEGs only */
-    @RequestMapping(value="/temporary-jpeg-image-{id}.htm", 
-            produces = MediaType.IMAGE_JPEG_VALUE, method = RequestMethod.GET)
+    @GetMapping(value="/temporary-jpeg-image-{id}.htm", 
+            produces = IMAGE_JPEG_VALUE)
     @ResponseBody
     @Transactional(readOnly = true)
     // this annotation is needed or else I get:
@@ -77,7 +71,7 @@ public class FileUploadController {
     }
     
     /** This is a test that shows any image type */
-    @RequestMapping(value="/temporary-image-{id}.htm", method = RequestMethod.GET)
+    @GetMapping(value="/temporary-image-{id}.htm")
     @Transactional(readOnly = true)
     public ResponseEntity<byte[]> showTemporaryImage(@PathVariable Long id) {
         final HttpHeaders responseHeaders = new HttpHeaders();
@@ -86,7 +80,7 @@ public class FileUploadController {
                 responseHeaders, HttpStatus.OK);
     }
     
-    @RequestMapping(value="/upload-save.htm", method= RequestMethod.POST) 
+    @PostMapping(value="/upload-save.htm") 
     @ResponseBody
     @Transactional
     public String saveImage(@RequestParam Long tempImageId) {
--- webapp/src/main/java/chiralsoftware/webapp/controllers/ImageUtilities.java
+++ webapp/src/main/java/chiralsoftware/webapp/controllers/ImageUtilities.java
@@ -1,19 +1,25 @@
 package chiralsoftware.webapp.controllers;
 
 import java.awt.AlphaComposite;
+import static java.awt.AlphaComposite.CLEAR;
+import static java.awt.AlphaComposite.SRC_OVER;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import static java.awt.RenderingHints.KEY_INTERPOLATION;
+import static java.awt.RenderingHints.KEY_RENDERING;
+import static java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR;
+import static java.awt.RenderingHints.VALUE_RENDER_QUALITY;
 import java.awt.image.BufferedImage;
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import static javax.imageio.ImageIO.write;
 
 /**
  * Some helpful utilities for processing uploaded image
  * 
- * @author hh
  */
 public final class ImageUtilities {
     private static final Logger LOG = Logger.getLogger(ImageUtilities.class.getName());
@@ -27,40 +33,33 @@ public final class ImageUtilities {
     
     public static byte[] makeIcon(InputStream is) throws IOException {
         final BufferedImage bufferedImage = ImageIO.read(is);
-        final BufferedImage result = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        final BufferedImage result = new BufferedImage(WIDTH, HEIGHT, TYPE_INT_ARGB);
         final Graphics2D g = result.createGraphics();
-        g.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+        g.setComposite(AlphaComposite.getInstance(CLEAR));
         g.fillRect(0,0,result.getWidth(), result.getHeight());
-        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+        g.setComposite(AlphaComposite.getInstance(SRC_OVER));
         
         // find out if we need to scale things
         // we will scale small images UP if we need to
         final float xScale = (float) bufferedImage.getWidth() / WIDTH;
         final float yScale = (float) bufferedImage.getHeight() / HEIGHT;
         LOG.info("The source image width / height: " + bufferedImage.getWidth() + " / " + bufferedImage.getHeight());
-        LOG.info("The xScale / yScale is: " + xScale + " / " + yScale);
         
         final float scale = xScale > yScale ? xScale : yScale;
-        LOG.info("And so I will use this scale: " + scale);
         
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(KEY_INTERPOLATION, VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(KEY_RENDERING, VALUE_RENDER_QUALITY);
 
         final int newWidth = Math.round(bufferedImage.getWidth() / scale);
         final int newHeight = Math.round(bufferedImage.getHeight() / scale);
         final int newXPos = (result.getWidth() - newWidth) / 2;
         final int newYPos = (result.getHeight() - newHeight) / 2;
-//        LOG.info("The new width: " + newWidth);
-//        LOG.info("The new height: " + newHeight);
-//        LOG.info("The new xPos: " + newXPos);
-//        LOG.info("The new yPos: " + newYPos);
-//        LOG.info("Now I am drawing");
+
         g.drawImage(bufferedImage, newXPos, newYPos, newWidth, newHeight, null);
         g.dispose();
         
-//        ImageIO.write(result, "PNG", new File("/tmp/test-output.png"));
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(result, "PNG", baos);
+        write(result, "PNG", baos);
         baos.close();
         return baos.toByteArray();
     }
