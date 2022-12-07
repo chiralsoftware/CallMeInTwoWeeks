a7d29b74d4f72d5697dc69d4f89cfef2fea86fcb
hh <eric@chiralsoftware.com>
2022-12-06 18:12:46 -0800

    login as user and admin now work with Spring Security 6. This is still not ready for production use.

--- cmi2w/src/main/java/chiralsoftware/cmi2w/MyWebSecurityConfig.java
+++ cmi2w/src/main/java/chiralsoftware/cmi2w/MyWebSecurityConfig.java
@@ -1,8 +1,10 @@
 package chiralsoftware.cmi2w;
 
+import chiralsoftware.cmi2w.security.MyAuthenticationSuccessHandler;
 import java.util.logging.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -21,22 +23,38 @@ public class MyWebSecurityConfig {
     private static final Logger LOG = Logger.getLogger(MyWebSecurityConfig.class.getName());
     
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/style/**", "/", "/index.htm", "/signup").permitAll();
-        http.authorizeHttpRequests().
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests().requestMatchers("/style/**", "/", "/index.htm", "/login", "/signup").permitAll();
+//        http.authorizeHttpRequests().
 //                .csrf().disable()
-                requestMatchers("/secure/**").hasRole("USER").and().formLogin();
+//                requestMatchers("/secure/**").hasRole("USER").and().formLogin().loginPage("/login").successForwardUrl("/secure/index.htm");
         
-        http.authorizeHttpRequests().
-//                .csrf().disable()
-                requestMatchers("/admin/**").hasRole("ADMIN").and().formLogin();
+//        http.authorizeHttpRequests().
+//                requestMatchers("/admin/**").hasRole("ADMIN").and().formLogin().
+//                    defaultSuccessUrl("/admin/list-users.htm");
         
-        return http.build();
+//        return http.build();
+
+
+        return http.
+                authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/style/**", "/", "/index.htm", "/login", "/signup").permitAll();
+                    auth.requestMatchers("/secure/**").hasAnyRole("USER");
+//                    auth.anyRequest().authenticated(); 
+                    auth.requestMatchers("/admin/**").hasRole("ADMIN");
+                }).
+                formLogin(customizer -> { 
+//                    customizer.loginPage("/login"); // default page generated
+                    customizer.successHandler(new MyAuthenticationSuccessHandler());
+                    LOG.info("I set the new success handler!");
+                }).
+                build();
     }
     
     @Bean 
     public UserDetailsService userDetailsService() {
         final UserDetails user = User.withUsername("user").password("hello").authorities("user").build();
+        LOG.info("the fake user details service has been called!!!");
         return new InMemoryUserDetailsManager(user);
     }
     
--- cmi2w/src/main/java/chiralsoftware/cmi2w/security/MyAuthenticationSuccessHandler.java
+++ cmi2w/src/main/java/chiralsoftware/cmi2w/security/MyAuthenticationSuccessHandler.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import java.util.logging.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
@@ -19,6 +21,8 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public final class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private static final Logger LOG = Logger.getLogger(MyAuthenticationSuccessHandler.class.getName());
     
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    
     @Override
     public void onAuthenticationSuccess(HttpServletRequest hsr, HttpServletResponse httpServletResponse, Authentication a) 
             throws IOException, ServletException {
@@ -26,12 +30,14 @@ public final class MyAuthenticationSuccessHandler implements AuthenticationSucce
         LOG.info("Here are the roles: " + roles);
         if(roles.stream().anyMatch(s -> s.toLowerCase().endsWith("admin"))) { 
             LOG.info("Authentication success, user is an admin user.");
-            httpServletResponse.sendRedirect("admin/list-users.htm"); 
+//            httpServletResponse.sendRedirect("admin/list-users.htm"); 
+            redirectStrategy.sendRedirect(hsr, httpServletResponse, "admin/list-users.htm"); 
             return; 
         }
         if(roles.stream().anyMatch(s -> s.toLowerCase().endsWith("user"))) { 
             LOG.info("Authentication success. user is a normal user.");
-            httpServletResponse.sendRedirect("secure/index.htm"); 
+//            httpServletResponse.sendRedirect("secure/index.htm"); 
+            redirectStrategy.sendRedirect(hsr, httpServletResponse, "secure/index.htm"); 
             return; 
         }
         LOG.warning("Roles set was unexpected: " + roles);
