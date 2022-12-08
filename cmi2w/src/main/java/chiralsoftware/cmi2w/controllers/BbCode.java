9f747f2a465a44055187dc618d76f72b35b2293b
hh <eric@chiralsoftware.com>
2022-12-07 17:42:09 -0800

    trying to get native-image to work, running into a problem with BBProcessorFactory.getInstance()

--- cmi2w/src/main/java/chiralsoftware/cmi2w/controllers/BbCode.java
+++ cmi2w/src/main/java/chiralsoftware/cmi2w/controllers/BbCode.java
@@ -5,9 +5,11 @@ import org.kefirsf.bb.TextProcessor;
 import org.kefirsf.bb.conf.Configuration;
 
 /**
- * Holder for a BBCode processor. This should be moved in to a bean probably
- *
- * @author hh
+ * Holder for a BBCode processor. This should be moved in to a bean probably.
+ * This is failing with native-image because:
+ * Caused by: org.kefirsf.bb.TextProcessorFactoryException: Can't find or open default configuration resource.
+* So do something to create a configuration instance so it doesn't need to search for it
+ * 
  */
 public final class BbCode {
     
